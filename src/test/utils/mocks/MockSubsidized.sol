09adfc7006a7aafaa850845dc8fc379cbfa3306b
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-07-10 12:25:29 -0400

    Update and rename src/tests/mocks/MockSubsidized.sol to src/test/utils/mocks/MockSubsidized.sol
    🥢 rename

--- src/test/utils/mocks/MockSubsidized.sol
+++ src/test/utils/mocks/MockSubsidized.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.4;
 
-import {Subsidized} from "../../mods/Subsidized.sol";
+import {Subsidized} from "../../../mods/Subsidized.sol";
 
 contract MockSubsidized is Subsidized {
     uint256 counter;
