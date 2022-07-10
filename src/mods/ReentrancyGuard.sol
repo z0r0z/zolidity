dd3a383f624b917706447e9adb3dcd80256db651
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-07-10 16:07:53 -0400

    Update ReentrancyGuard.sol
    style

--- src/mods/ReentrancyGuard.sol
+++ src/mods/ReentrancyGuard.sol
@@ -5,7 +5,8 @@ error Reentrancy();
 
 /// @notice Reentrancy protection for smart contracts.
 /// @author z0r0z.eth
-/// @author Modified from Seaport (https://github.com/ProjectOpenSea/seaport/blob/main/contracts/lib/ReentrancyGuard.sol)
+/// @author Modified from Seaport 
+///         (https://github.com/ProjectOpenSea/seaport/blob/main/contracts/lib/ReentrancyGuard.sol)
 abstract contract ReentrancyGuard {
     uint256 private locked = 1;
 
