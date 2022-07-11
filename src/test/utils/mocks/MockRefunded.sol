78c4511e73b5ba2dd7f1e6d949fac2a5bfad6abf
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-07-10 22:34:33 -0400

    Update MockRefunded.sol
    🥢

--- src/test/utils/mocks/MockRefunded.sol
+++ src/test/utils/mocks/MockRefunded.sol
@@ -3,22 +3,23 @@ pragma solidity >=0.8.4;
 
 import {Refunded} from "../../../mods/Subsidized.sol";
 
+/// @notice Mock Refunded contract.
 contract MockRefunded is Refunded {
-    uint256 counter;
+    uint256 private counter;
 
     receive() external payable {}
 
-    // Refunded transaction, where *most* of the gas-cost is returned to the caller (tx.origin).
+    /// @dev Refunded transaction, where *most* of the gas-cost is returned to the caller (tx.origin).
     function foo() public isRefunded {
         expensive();
     }
 
-    // Unrefunded transaction, for testing & comparison.
+    /// @dev Unrefunded transaction, for testing & comparison.
     function bar() public {
         expensive();
     }
 
-    // A gas-expensive function.
+    /// @dev A gas-expensive function.
     function expensive() public {
         for (uint256 i=0; i < 50; i++){
             counter++;
