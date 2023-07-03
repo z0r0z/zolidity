fefe0add4a96eb699d0fa233736acc1826bdd5ea
ross <92001561+z0r0z@users.noreply.github.com>
2023-07-03 15:32:51 +0000

    🥢Immutables ERC20

--- src/tkn/ERC20.sol
+++ src/tkn/ERC20.sol
@@ -4,13 +4,45 @@ pragma solidity 0.8.20;
 /// @dev ERC20 token
 /// @author Zolidity
 abstract contract ERC20 {
+    /// -----------------------------------------------------------------------
+    /// ERC20 events
+    /// -----------------------------------------------------------------------
+
     event Approval(address indexed from, address indexed to, uint256 amt);
     event Transfer(address indexed from, address indexed to, uint256 amt);
+
+    /// -----------------------------------------------------------------------
+    /// ERC20 storage
+    /// -----------------------------------------------------------------------
 
     mapping(address => mapping(address => uint256)) public allowance;
     mapping(address => uint256) public balanceOf;
 
     uint256 public totalSupply;
+
+    /// -----------------------------------------------------------------------
+    /// ERC20 metadata immutables
+    /// -----------------------------------------------------------------------
+
+    uint256 public constant decimals = 18;
+
+    string public name = string(abi.encodePacked(_immutable(0)));
+    string public symbol = string(abi.encodePacked(_immutable(20)));
+
+    function _immutable(uint256 pos) internal pure returns (uint256 out) {
+        assembly {
+            out :=
+                calldataload(
+                    add(
+                        sub(calldatasize(), add(shr(240, calldataload(sub(calldatasize(), 2))), 2)), pos
+                    )
+                )
+        }
+    }
+
+    /// -----------------------------------------------------------------------
+    /// ERC20 logic
+    /// -----------------------------------------------------------------------
 
     function approve(address to, uint256 amt) external payable returns (bool) {
         allowance[msg.sender][to] = amt;
@@ -36,6 +68,10 @@ abstract contract ERC20 {
         emit Transfer(from, to, amt);
         return true;
     }
+
+    /// -----------------------------------------------------------------------
+    /// Internal mint/burn logic
+    /// -----------------------------------------------------------------------
 
     function _mint(address to, uint256 amt) internal {
         totalSupply += amt;
