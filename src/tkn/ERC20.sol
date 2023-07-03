6e19b2e29fd6b968cbc0921d02c66db9f395950b
ross <92001561+z0r0z@users.noreply.github.com>
2023-07-03 14:56:36 +0000

    🥢 Finish up ERC20

--- src/tkn/ERC20.sol
+++ src/tkn/ERC20.sol
@@ -4,14 +4,19 @@ pragma solidity 0.8.20;
 /// @dev ERC20 token
 /// @author Zolidity
 abstract contract ERC20 {
+    event Approval(address indexed from, address indexed to, uint256 amt);
     event Transfer(address indexed from, address indexed to, uint256 amt);
 
-    mapping(address from => mapping(address to => uint256)) public allowance;
-    mapping(address to => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    mapping(address => uint256) public balanceOf;
 
     uint256 public totalSupply;
 
-    function approve(address to, uint256 amt) external payable returns (bool) {}
+    function approve(address to, uint256 amt) external payable returns (bool) {
+        allowance[msg.sender][to] = amt;
+        emit Approval(msg.sender, to, amt);
+        return true;
+    }
 
     function transfer(address to, uint256 amt) external payable returns (bool) {
         balanceOf[msg.sender] -= amt;
@@ -22,11 +27,29 @@ abstract contract ERC20 {
         return true;
     }
 
+    function transferFrom(address from, address to, uint256 amt) external payable returns (bool) {
+        allowance[from][msg.sender] -= amt;
+        balanceOf[from] -= amt;
+        unchecked {
+            balanceOf[to] += amt;
+        }
+        emit Transfer(from, to, amt);
+        return true;
+    }
+
     function _mint(address to, uint256 amt) internal {
         totalSupply += amt;
         unchecked {
             balanceOf[to] += amt;
         }
         emit Transfer(address(0), to, amt);
+    }
+
+    function _burn(address from, uint256 amt) internal {
+        balanceOf[from] -= amt;
+        unchecked {
+            totalSupply -= amt;
+        }
+        emit Transfer(from, address(0), amt);
     }
 }
