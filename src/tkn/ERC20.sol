0c83cdc7f98c9afbf449f163bb19b4b074b2275d
ross <92001561+z0r0z@users.noreply.github.com>
2023-07-04 16:51:32 +0000

    🪥 Brush comment aesthetics

--- src/tkn/ERC20.sol
+++ src/tkn/ERC20.sol
@@ -4,21 +4,15 @@ pragma solidity 0.8.20;
 /// @dev ERC20 token
 /// @author Zolidity
 abstract contract ERC20 {
-    // Initialization
-    constructor(bytes32 meta) {
-        _meta = meta;
-    }
-
-    // ERC20 Events
     event Transfer(address indexed from, address indexed to, uint amt);
     event Approval(address indexed from, address indexed to, uint amt);
 
-    // ERC20 Storage
+    // STORAGE
     mapping(address usr => mapping(address to => uint)) public allowance;
     mapping(address usr => uint) public balanceOf;
     uint public totalSupply;
 
-    // ERC20 Metadata
+    // METADATA
     uint public constant decimals = 18;
     bytes32 immutable _meta;
 
@@ -40,7 +34,12 @@ abstract contract ERC20 {
         return string(result);
     }
 
-    // ERC20 Logic
+    // CREATION
+    constructor(bytes32 meta) {
+        _meta = meta;
+    }
+
+    // LOGIC
     function approve(address to, uint amt) public payable returns (bool) {
         allowance[msg.sender][to] = amt;
         emit Approval(msg.sender, to, amt);
@@ -66,7 +65,7 @@ abstract contract ERC20 {
         return true;
     }
 
-    // Mint & Burn
+    // EXT: MINT/BURN
     function _mint(address to, uint amt) internal {
         totalSupply += amt;
         unchecked {
