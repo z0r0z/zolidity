c03577a2e1e59b3dbff2c9a3353689e2ddc209da
ross <92001561+z0r0z@users.noreply.github.com>
2025-03-19 08:39:21 +0000

    🔎 test ierc20

--- src/interfaces/IERC20.sol
+++ src/interfaces/IERC20.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.27;
 
 /// @notice Minimal fungible token interface.
 /// @author Zolidity (https://github.com/z0r0z/zolidity/blob/main/src/interfaces/IERC20.sol)
+/// @dev Boolean is not returned so that this interface works with non-conforming tokens too.
 interface IERC20 {
     function approve(address, uint256) external;
     function transfer(address, uint256) external;
